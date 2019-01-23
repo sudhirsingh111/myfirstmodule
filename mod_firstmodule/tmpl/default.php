07c0514c3d96476d7a7766cd460c1d302c7c19fc
sudhirsingh111 <46950151+sudhirsingh111@users.noreply.github.com>
2019-01-23 09:45:12 +0530

    this is first module in joomla
    this is first module in joomla  that select list of article and shows article details on front end

---
+++ mod_firstmodule/tmpl/default.php
@@ -0,0 +1,18 @@
+<?php
+   /**
+      * @package Joomla.Site
+      * @subpackage mod_firstmodule
+      * @copyright Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+      * @license GNU General Public License version 2 or later; see LICENSE.txt
+   */
+ 
+defined('_JEXEC') or die;
+
+
+$aa=typeof($mytextvalue1);
+echo $aa;
+?>
+
+
+	<p class="danger">Welcome to joomdev!!!!!</p>
+	 
